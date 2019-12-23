1f109f67f2f0c490045bfdff4a6e729db94099d9
jaesung lim <lim4057@naver.com>
2019-12-24 00:41:24 +0900

    basic Feature add

---
+++ src/test/java/me/jslim/demospringsecurityform/account/AccountControllerTest.java
@@ -0,0 +1,107 @@
+package me.jslim.demospringsecurityform.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @WithAnonymousUser
+    public void index_anonymous() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUser
+    public void index_user() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUser
+    public void admin_user() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void admin_admin() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    public void login_success() throws Exception {
+        String name = "jaesung";
+        String password = "123";
+
+        Account user = this.createUser(name,password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_success2() throws Exception {
+        String name = "jaesung";
+        String password = "123";
+
+        Account user = this.createUser(name,password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    public void login_fail() throws Exception {
+        String name = "jaesung";
+        String password = "123";
+
+        Account user = this.createUser(name,password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password("111"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String name, String password) {
+        Account account = new Account();
+        account.setUsername(name);
+        account.setPassword(password);
+        account.setRole("USER");
+        accountService.createNew(account);
+        return account;
+    }
+}
---
+++ src/test/java/me/jslim/demospringsecurityform/account/WithUser.java
@@ -0,0 +1,11 @@
+package me.jslim.demospringsecurityform.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "jaesung", roles = "USER")
+public @interface WithUser {
+}
